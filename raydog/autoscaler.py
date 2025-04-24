1440d7fd0b41be0089da9570dc467947084421ae
Winston Bond <winston.bond@yellowdog.ai>
2025-04-24 17:55:12 +0100

    start to fill in the node provider

--- raydog/autoscaler.py
+++ raydog/autoscaler.py
@@ -1,16 +1,69 @@
-
 import logging
+import dotenv
+import os
+import shortuuid
+
 from typing import Any, Dict, List, Optional
+from datetime import timedelta
 
 from ray.autoscaler.node_provider import NodeProvider
+
+from yellowdog_client.model import ApiKey, ServicesSchema
+from yellowdog_client.platform_client import PlatformClient
+
+from raydog.raydog import RayDogCluster
+
+logger = logging.getLogger(__name__)
 
 class RayDogNodeProvider(NodeProvider):
 
     def __init__(self, provider_config: Dict[str, Any], cluster_name: str) -> None:
+        print("RayDogNodeProvider", cluster_name, provider_config)
         self.provider_config = provider_config
         self.cluster_name = cluster_name
+
         self._internal_ip_cache: Dict[str, str] = {}
         self._external_ip_cache: Dict[str, str] = {}
+
+        # Read any extra environment variables from a file
+        dotenv.load_dotenv(verbose=True, override=True)
+
+        # Connect to YellowDog
+        self._api_url = os.getenv("YD_API_URL")
+        self._api_key_id = os.getenv("YD_API_KEY_ID")
+        self._api_key_secret = os.getenv("YD_API_KEY_SECRET")
+
+        self._ydclient = PlatformClient.create(
+            ServicesSchema(defaultUrl=self._api_url),
+            ApiKey(
+                os.getenv(self._api_key_id),
+                os.getenv(self._api_key_secret),
+            ),
+        )
+
+    @staticmethod
+    def _read_file(filename):
+        """Read the contents of a file"""
+        with open(filename) as f:
+            return f.read()
+
+    def _get_lifetime(self):
+        """Get the cluster lifetime frmo the config. If the lifetime ends with 'd' its in days, 
+           'm' is minutes, 's' (or nothing) is seconds
+        """
+        lifetime = self.provider_config.get('lifetime')
+        if not lifetime:
+            return None
+ 
+        lastchar = lifetime[-1].lower()
+        if lastchar == 'd':
+            return timedelta(days=int(lifetime[0:-1]))
+        elif lastchar == 'm':
+            return timedelta(minutes=int(lifetime[0:-1]))
+        elif lastchar == 's':
+            return timedelta(seconds=int(lifetime[0:-1]))
+        else:
+            return timedelta(seconds=int(lifetime))
 
     def non_terminated_nodes(self, tag_filters: Dict[str, str]) -> List[str]:
         """Return a list of node ids filtered by the specified tags dict.
@@ -31,6 +84,7 @@ class RayDogNodeProvider(NodeProvider):
 
         """
         print("non_terminated_nodes")
+        return []
 
     def is_running(self, node_id: str) -> bool:
         """Return whether the specified node is running."""
@@ -56,16 +110,38 @@ class RayDogNodeProvider(NodeProvider):
         self, node_config: Dict[str, Any], tags: Dict[str, str], count: int
     ) -> Optional[Dict[str, Any]]:
         """Creates a number of nodes within the namespace.
-
-        Optionally returns a mapping from created node ids to node metadata.
-
-        Optionally may throw a
-        ray.autoscaler.node_launch_exception.NodeLaunchException which the
-        autoscaler may use to provide additional functionality such as
-        observability.
-
         """
-        print("create_node", node_config)
+        node_type = tags['ray-node-type']
+        node_name = tags['ray-node-name']
+
+        userdata = self._read_file(node_config['userdata'])
+        cmptreqt = node_config['compute_requirement_template']
+        imagesid = node_config['images_id']
+
+        if node_type == 'head':
+            namespace = self.provider_config['namespace']
+
+            self._raydog_cluster = RayDogCluster(
+                yd_application_key_id=self._api_key_id,
+                yd_application_key_secret=self._api_key_secret,
+                yd_platform_api_url=self._api_url,
+
+                cluster_name=self.cluster_name,
+                cluster_namespace=namespace,
+                cluster_tag="my-ray-tag",
+                cluster_lifetime=self._get_lifetime(),
+
+                head_node_compute_requirement_template_id=cmptreqt,
+                head_node_images_id=imagesid,
+                head_node_userdata=userdata
+            )
+
+            self._raydog_cluster.build()
+        else:
+            pass
+
+
+        print("create_node", node_config, tags, count)
 
     def set_node_tags(self, node_id: str, tags: Dict[str, str]) -> None:
         """Sets the tag values (string dict) for the specified node."""
@@ -79,21 +155,14 @@ class RayDogNodeProvider(NodeProvider):
         """
         print("terminate_node", node_id)
 
-    @staticmethod
-    def bootstrap_config(cluster_config: Dict[str, Any]) -> Dict[str, Any]:
-        """Bootstraps the cluster config by adding env defaults if needed."""
-        print("bootstrap_config", cluster_config)
-        return cluster_config
-
     def prepare_for_head_node(self, cluster_config: Dict[str, Any]) -> Dict[str, Any]:
         """Returns a new cluster config with custom configs for head node."""
         print("prepare_for_head_node", cluster_config)
         return cluster_config
 
+
     @staticmethod
-    def fillout_available_node_types_resources(
-        cluster_config: Dict[str, Any]
-    ) -> Dict[str, Any]:
-        """Fills out missing "resources" field for available_node_types."""
-        print("fillout_available_node_types_resources", cluster_config)
+    def bootstrap_config(cluster_config: Dict[str, Any]) -> Dict[str, Any]:
+        """Bootstraps the cluster config by adding env defaults if needed."""
+        print("bootstrap_config", cluster_config)
         return cluster_config
