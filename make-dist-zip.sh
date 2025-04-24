d9d8f5a1079644f9bfdfcc1e7b4536fe8d67473f
pwt <1089749+pwt@users.noreply.github.com>
2025-04-24 10:46:05 +0100

    Add 'version.txt' to the dist zip file

--- make-dist-zip.sh
+++ make-dist-zip.sh
@@ -4,24 +4,28 @@
 
 DIST="dist"
 RAYDOG="raydog"
+COMMIT=$(git rev-parse --short HEAD)
+VERSION_FILE="version.txt"
 
 rm -rf "${DIST:?}/$RAYDOG"
 mkdir -p "$DIST/$RAYDOG"
+
+echo "Commit ID: $COMMIT" >> "$DIST/$RAYDOG/$VERSION_FILE"
 
 rm -rf raydog/__pycache__
 
 for ITEM in README.md \
             usage-example.py \
             raydog \
-            private_key
+            private-key
 do
   cp -r $ITEM $DIST/$RAYDOG/.
 done
 
-chmod 0600 $DIST/$RAYDOG/private_key
+chmod 0600 $DIST/$RAYDOG/private-key
 
 cd $DIST || exit
-ZIPFILE=raydog-"$(git rev-parse --short HEAD)".zip
+ZIPFILE=raydog-$COMMIT.zip
 echo "Creating $ZIPFILE ..."
 zip -r $ZIPFILE $RAYDOG
 echo "Done"
