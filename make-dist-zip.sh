8ebb2580d1cfc1b7b4cdd69268c9439879f4ba16
pwt <1089749+pwt@users.noreply.github.com>
2025-05-04 09:31:12 +0100

    Add CHANGELOG to zip distribution

--- make-dist-zip.sh
+++ make-dist-zip.sh
@@ -6,12 +6,14 @@ DIST="dist"
 RAYDOG="raydog"
 COMMIT=$(git rev-parse --short HEAD)
 VERSION_FILE="version.txt"
+CHANGELOG_FILE="CHANGELOG.txt"
 DATESTAMP=$(date -u "+%Y%m%d%H%M%S")
 
 rm -rf "${DIST:?}/$RAYDOG"
 mkdir -p "$DIST/$RAYDOG"
 
 echo "Commit ID: $COMMIT" > "$DIST/$RAYDOG/$VERSION_FILE"
+git log --oneline > "$DIST/$RAYDOG/$CHANGELOG_FILE"
 
 rm -rf raydog/__pycache__
 
@@ -19,7 +21,8 @@ for ITEM in README.md \
             usage-example.py \
             raydog \
             private-key \
-            jupyter-demo.ipynb
+            jupyter-demo.ipynb \
+            CHANGELOG.txt
 do
   cp -r $ITEM $DIST/$RAYDOG/.
 done
