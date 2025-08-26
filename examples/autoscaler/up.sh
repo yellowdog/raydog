56235829d334b2eed5adbd0d142b34afa39b680d
pwt <1089749+pwt@users.noreply.github.com>
2025-08-26 14:55:31 +0100

    Fix file path

--- examples/autoscaler/up.sh
+++ examples/autoscaler/up.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Temporarily upload the current version of the autoscaler to S3
-aws s3 cp ../..//src/yellowdog_ray/raydog/autoscaler.py \
+aws s3 cp ../../src/yellowdog_ray/raydog/autoscaler.py \
           s3://tech.yellowdog.devsandbox.raydog.autoscaler/autoscaler.py
 ray disable-usage-stats
 ray --logging-level=info up --yes --no-config-cache raydog.yaml
